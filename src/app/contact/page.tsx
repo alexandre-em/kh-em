aeed3ed61820c27611e6a64c7aba14343a08cbe9
Alexandre <em_alexandre@yahoo.co.jp>
2023-12-04 18:32:25 +0100

    Fix contact page overflow bug

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -32,60 +32,58 @@ export default function Contact() {
   );
 
   return (
-    <>
+    <main className="h-full overflow-y-auto">
       <Navbar />
-      <main>
-        <div className="m-5 min-h-[calc(100vh-165px)] flex flex-col items-center justify-center">
-          <div className="font-black text-gray-800 text-3xl flex flex-wrap">
-            <span>Vous pouvez me contacter&nbsp;</span>
-            <span className="text-[#536DFE]">ici</span>
-          </div>
-          <Linksgroup title={false} />
-          <Divider className="w-[80%]" />
-          <div className="font-black text-gray-800 text-2xl flex flex-wrap mt-3 mb-3">
-            <span>Mais également via ce&nbsp;</span>
-            <span className="text-[#536DFE]">formulaire&nbsp;</span>
-          </div>
-          <form ref={formRef} className="bg-white mt-3 p-5 rounded-2xl" onSubmit={handleSubmit}>
-            {mailStatus === 'SUCCESS' && (
-              <Alert severity="success">
-                <AlertTitle>Mail sent successfully !</AlertTitle>I will reply you as soon as possible
-              </Alert>
-            )}
-            {mailStatus === 'ERROR' && (
-              <Alert severity="error">
-                <AlertTitle>An error occured !</AlertTitle> Please try again in few minutes
-              </Alert>
-            )}
-            <TextField label="Name" required name="name" variant="outlined" fullWidth sx={{ mb: 2, mt: 3 }} />
-            <TextField label="Email" name="email" required variant="outlined" type="email" sx={{ mb: 3 }} fullWidth />
-            <TextField
-              required
-              multiline
-              fullWidth
-              id="outlined-multiline-static"
-              label="Message"
-              name="message"
-              rows={5}
-              maxRows={10}
-              placeholder="You can write your message here..."
-            />
-            <Button variant="contained" className="bg-[#2196f3] mt-5" type="submit" disabled={mailStatus === 'LOADING'}>
-              {mailStatus === 'LOADING' ? 'Loading...' : 'Send mail'}
-              {mailStatus === 'LOADING' && <CircularProgress className="ml-3" size={15} />}
-            </Button>
-          </form>
+      <div className="m-5 min-h-[calc(100vh-165px)] flex flex-col items-center justify-center">
+        <div className="font-black text-gray-800 text-3xl flex flex-wrap">
+          <span>Vous pouvez me contacter&nbsp;</span>
+          <span className="text-[#536DFE]">ici</span>
         </div>
-        <Image
-          src="https://storage.googleapis.com/khindelvert-af786.appspot.com/uploads/89f9e091-4a3b-4d5f-b15e-56fede75ee13/DSC00301_1000.png"
-          alt="image"
-          loading="lazy"
-          width={560}
-          height={760}
-          className="absolute bottom-0 right-0 z-[-10] w-full mix-blend-darken opacity-[15%] blur-md"
-        />
-      </main>
+        <Linksgroup title={false} />
+        <Divider className="w-[80%]" />
+        <div className="font-black text-gray-800 text-2xl flex flex-wrap mt-3 mb-3">
+          <span>Mais également via ce&nbsp;</span>
+          <span className="text-[#536DFE]">formulaire&nbsp;</span>
+        </div>
+        <form ref={formRef} className="bg-white mt-3 p-5 rounded-2xl" onSubmit={handleSubmit}>
+          {mailStatus === 'SUCCESS' && (
+            <Alert severity="success">
+              <AlertTitle>Mail sent successfully !</AlertTitle>I will reply you as soon as possible
+            </Alert>
+          )}
+          {mailStatus === 'ERROR' && (
+            <Alert severity="error">
+              <AlertTitle>An error occured !</AlertTitle> Please try again in few minutes
+            </Alert>
+          )}
+          <TextField label="Name" required name="name" variant="outlined" fullWidth sx={{ mb: 2, mt: 3 }} />
+          <TextField label="Email" name="email" required variant="outlined" type="email" sx={{ mb: 3 }} fullWidth />
+          <TextField
+            required
+            multiline
+            fullWidth
+            id="outlined-multiline-static"
+            label="Message"
+            name="message"
+            rows={5}
+            maxRows={10}
+            placeholder="You can write your message here..."
+          />
+          <Button variant="contained" className="bg-[#2196f3] mt-5" type="submit" disabled={mailStatus === 'LOADING'}>
+            {mailStatus === 'LOADING' ? 'Loading...' : 'Send mail'}
+            {mailStatus === 'LOADING' && <CircularProgress className="ml-3" size={15} />}
+          </Button>
+        </form>
+      </div>
+      <Image
+        src="https://storage.googleapis.com/khindelvert-af786.appspot.com/uploads/89f9e091-4a3b-4d5f-b15e-56fede75ee13/DSC00301_1000.png"
+        alt="image"
+        loading="lazy"
+        width={560}
+        height={760}
+        className="absolute bottom-0 right-0 z-[-10] w-full mix-blend-darken opacity-[15%] blur-md"
+      />
       <Footer />
-    </>
+    </main>
   );
 }
